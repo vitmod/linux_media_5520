dbc4920246c4b0d8c8f30269a6fbefd8ffeac652
CrazyCat <crazycat69@narod.ru>
2016-02-25 23:32:18 +0200

    cx231xx: Fix I2C bus num for TBS 5280, 5281, 5990.

--- drivers/media/usb/cx231xx/cx231xx-dvb.c
+++ drivers/media/usb/cx231xx/cx231xx-dvb.c
@@ -1063,6 +1063,7 @@ static int dvb_init(struct cx231xx *dev)
 		struct si2168_config si2168_config;
 		struct si2157_config si2157_config;
 
+		demod_i2c = cx231xx_get_i2c_adap(dev, dev->board.demod_i2c_master+i);
 		/* attach frontend */
 		si2168_config.i2c_adapter = &adapter;
 		si2168_config.fe = &dev->dvb[i]->frontend;
@@ -1118,6 +1119,7 @@ static int dvb_init(struct cx231xx *dev)
 	}
 	case CX231XX_BOARD_TBS_5990:
 	{
+		demod_i2c = cx231xx_get_i2c_adap(dev, dev->board.demod_i2c_master+i);
 		dev->dvb[i]->frontend = dvb_attach(tas2101_attach, &tbs5990_tas2101_cfg[i],
 						demod_i2c);
 
